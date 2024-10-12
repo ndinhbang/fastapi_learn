bb209cc6a6e35006c42323e07bf2dfb5fee52df6
Dinh Bang <ndinhbang111@gmail.com>
2024-10-12 11:38:41 +0000

    improve model declaration

--- app/models.py
+++ app/models.py
@@ -1,18 +1,23 @@
 from datetime import datetime
-from sqlalchemy import func
+from sqlalchemy import String, func
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database import Base
 
 
+# https://docs.sqlalchemy.org/en/20/orm/quickstart.html#declare-models
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
-    username: Mapped[str] = mapped_column(index=True, unique=True)
-    email: Mapped[str] = mapped_column(index=True, unique=True)
-    password: Mapped[str]
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String(125), index=True, unique=True)
+    email: Mapped[str] = mapped_column(String(125), index=True, unique=True)
+    password: Mapped[str] = mapped_column(String(64))
 
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
         server_default=func.now(), onupdate=func.now()
     )
+    deleted_at: Mapped[datetime | None] = mapped_column(nullable=True)
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, username={self.username!r}, email={self.email!r})"
